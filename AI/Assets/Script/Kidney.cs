07190f0cb14711cdee26b3ef6ad6fe148cf865eb
muzhou0000 <55180633+muzhou0000@users.noreply.github.com>
2020-04-23 15:55:54 +0800

    網頁

---
+++ AI/Assets/Script/Kidney.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kidney : MonoBehaviour
+{
+    public static bool complate;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "knife")
+        {
+            print("碰到腰子");
+            complate = true;
+        }
+    }
+}
